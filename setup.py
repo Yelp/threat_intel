4aa09426c1feafb541b2e40cd3c543d7f3a5aba0
jsendor <jsendor@yelp.com>
2015-02-03 17:48:09 +0000

    Initial project import based on OSXCollector Output Filters

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="threat_intel",
+    version="0.0.1",
+    author="Yelp Security",
+    author_email="opensource@yelp.com",
+    description="Collection of the API calls for various threat intel feeds.",
+    packages=["threat_intel"],
+)
---
+++ threat_intel/exceptions.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# All exceptions thrown by the threat_intel module
+#
+
+class InvalidRequestError(Exception):
+
+    """Raised by MultiRequest when it can't figure out how to make a request."""
+    pass
