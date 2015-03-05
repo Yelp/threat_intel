d29c174a6b3d82d2cac98ecc09f74d9d0ee830f6
Marina bt3 <marina@yelp.com>
2015-03-05 15:30:27 -0800

    Modify setup.py to find util package in pypi.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="threat_intel",
-    version='0.0.1a2',
+    version='0.0.1a3',
     provides=['threat_intel'],
     author="Yelp Security",
     url='https://github.com/Yelp/threat_intel',
@@ -13,7 +13,7 @@ setup(
     license='Copyright 2015 Yelp',
     author_email="team-security@yelp.com",
     description="Collection of the API calls for various threat intel feeds.",
-    packages=['threat_intel', 'threat_intel.util'],
+    packages=['threat_intel', 'threat_intel.util', 'util_intel.util.api_cache'],
     install_requires=[
         "grequests==0.2.0",
         "simplejson==3.6.5",
