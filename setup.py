99b948e2c07dd06fc64268f212cabb05a9bf05d2
Marina bt3 <marina@yelp.com>
2015-03-05 13:29:45 -0800

    Fixing setup after travisci failure

--- setup.py
+++ setup.py
@@ -4,23 +4,20 @@ from __future__ import absolute_import
 from setuptools import find_packages
 from setuptools import setup
 
-import threat_intel.threat_intel
 
 setup(
     name="threat_intel",
-    version=threat_intel.__version__,
+    version='0.0.1a2',
     provides=['threat_intel'],
     author="Yelp Security",
     url='https://github.com/Yelp/threat_intel',
     setup_requires='setuptools',
-    license='Copyright 2014 Yelp',
-    author_email="opensource@yelp.com",
+    license='Copyright 2015 Yelp',
+    author_email="team-security@yelp.com",
     description="Collection of the API calls for various threat intel feeds.",
     packages=find_packages(exclude='tests*'),
-    include_package_data=True,
     install_requires=[
         "grequests==0.2.0",
         "simplejson==3.6.5",
-        "virtualenv",
     ],
 )
