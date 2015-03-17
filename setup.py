a3dbd35684fae474c9d6485b9cb3aac5b1b27cc2
Marina bt3 <marina@yelp.com>
2015-03-17 09:56:44 -0700

    changing setup.py version after bug fix was merged (this is the same version to appear at pypi).

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="threat_intel",
-    version='0.0.8',
+    version='0.0.9',
     provides=['threat_intel'],
     author="Yelp Security",
     url='https://github.com/Yelp/threat_intel',
