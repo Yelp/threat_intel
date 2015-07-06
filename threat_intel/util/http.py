920c0bd5a5edb49bfb5e02d46400085e0dfd95bc
Ivan Leichtling <ivanlei@yelp.com>
2015-07-06 09:13:28 -0700

    Small comment update

--- threat_intel/util/http.py
+++ threat_intel/util/http.py
@@ -4,6 +4,7 @@
 #
 # RateLimiter helps to only make a certain number of calls per second.
 # MultiRequest wraps grequests and issues multiple requests at once with an easy to use interface.
+# SSLAdapter helps force use of the highest possible version of TLS.
 #
 import ssl
 import time
