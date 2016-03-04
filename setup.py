5dda6a457b93237e88094db9ca95d1ee6b1fcb23
jsendor <jsendor@yelp.com>
2016-03-04 14:22:36 -0800

    Bumping the version to 0.1.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="threat_intel",
-    version='0.1.11',
+    version='0.1.12',
     provides=['threat_intel'],
     author="Yelp Security",
     url='https://github.com/Yelp/threat_intel',
