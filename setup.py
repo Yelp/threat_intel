561a1c8880c90cdca94c1ad5ffd13918c5e97310
Quentin Long <qlo@yelp.com>
2016-08-08 14:15:42 -0700

    Added -r requirements.txt to requirements-dev.txt, bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="threat_intel",
-    version='0.1.17',
+    version='0.1.18',
     provides=['threat_intel'],
     author="Yelp Security",
     url='https://github.com/Yelp/threat_intel',
