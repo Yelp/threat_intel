f8731042ac28b4ec243135c1f331c878225642d4
Jakub Jan Sendor <jsendor@yelp.com>
2016-10-28 16:24:07 -0700

    Hey, it's 2016

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="Yelp Security",
     url='https://github.com/Yelp/threat_intel',
     setup_requires='setuptools',
-    license='Copyright 2015 Yelp',
+    license='Copyright 2016 Yelp',
     author_email="opensource@yelp.com",
     description="Collection of the API calls for various threat intel feeds.",
     packages=find_packages(),
