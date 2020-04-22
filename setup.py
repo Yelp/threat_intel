97cf3028682d8f3fef62f14318770577f5018bfb
Yonny Tonui <ytonui@yelp.com>
2020-04-22 15:19:55 +0100

    upgrades threat_intel virustotal to version 3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="threat_intel",
-    version='0.1.30',
+    version='1.0',
     provides=['threat_intel'],
     author="Yelp Security",
     url='https://github.com/Yelp/threat_intel',
