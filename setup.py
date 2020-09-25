fece1e020fac6bfbc201468caf5752da732b62e5
Yonny Tonui <ytonui@yelp.com>
2020-09-25 11:25:52 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="threat_intel",
-    version='0.2.0',
+    version='0.2.1',
     provides=['threat_intel'],
     author="Yelp Security",
     url='https://github.com/Yelp/threat_intel',
